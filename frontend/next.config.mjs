d59608d66cb29a2453fcb90f81574f9d5f84e5ce
Hpone07 <hpone.k@lumahealth.com>
2024-06-19 11:59:06 +0700

    optimize css and fonts

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["loremflickr.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,26 @@
-import { ApolloProvider } from '@apollo/client';
-import client from '../lib/apollo-client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect } from 'react';
+import { ApolloProvider } from "@apollo/client";
+import client from "../lib/apollo-client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.min.js');
+    import("bootstrap/dist/js/bootstrap.min.js");
   }, []);
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <div className={roboto.className}>
+        <Component {...pageProps} />
+      </div>
     </ApolloProvider>
   );
 }
